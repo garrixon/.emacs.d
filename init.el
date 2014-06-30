05b53f6fe18eef28ba1ab84b2b178cbfd2bd9bc5
michael <garrixon>
2014-06-29 21:07:31 -0400

    Cleaned up init.el

--- init.el
+++ init.el
@@ -12,13 +12,11 @@
 (require 'yasnippet)
 (yas-global-mode 1) 
 ;cedet
-(load-file "~/.emacs.d/cedet-1.1/common/cedet.el")
-(global-ede-mode 1)                      ; Enable the Project management system
-(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
-(global-srecode-minor-mode 1)            ; Enable template insertion menu
+(semantic-mode 1)
 ;keybindings for semantic
 (defun my-cedet-hook ()
   (local-set-key [(control ?c) (control ?a)] 'semantic-ia-complete-symbol)
   (local-set-key [(control ?c) (control ?c)] 'semantic-complete-analyze-inline))
 (add-hook 'c-mode-common-hook 'my-cedet-hook)
 (add-hook 'c++-mode-hook 'my-cedet-hook)
+
